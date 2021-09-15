1dec5acb9a84a5a0a611a1df04925141a6e8d7c3
bryanromeromtz <cryptomtz@hotmail.com>
2021-09-14 22:56:57 -0500

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -10,6 +10,19 @@ class Account extends Component {
 
   componentDidMount() {
 
+    const headerLinks = [
+      {
+        _id: 0,
+        title: 'Shop',
+        path: '/shop'
+      },
+      {
+        _id: 1,
+        title: 'Logout',
+        path: '/'
+      }
+    ]
+
     const navbarLinks = [
       {
         _id: 0,
@@ -20,12 +33,12 @@ class Account extends Component {
       {
         _id: 1,
         title: 'Account Information',
-        active: false,
+        active: true,
         component: <AccountInformation />
       }
     ]
 
-    this.props.setHeaderLinks([]);
+    this.props.setHeaderLinks(headerLinks);
     this.props.setNavbarLinks(navbarLinks);
   }
 
--- src/components/header-navbar/header.js
+++ src/components/header-navbar/header.js
@@ -12,7 +12,7 @@ class Header extends Component {
           {
             this.props.headerLinks.map((link, index) => {
               return (
-                <a className='header__link' key={index} onClick={() => console.log('trying to switch tab')}>
+                <a className='header__link' key={index} onClick={() => history.push(link.path)}>
                   {link.title}
                 </a>
               )
