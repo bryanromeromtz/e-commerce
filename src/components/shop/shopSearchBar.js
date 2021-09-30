458e32fdb4398baae29a70f8b4e581a9382796aa
bryanromeromtz <cryptomtz@hotmail.com>
2021-09-29 21:06:15 -0500

    search bar styles

--- src/components/shop/shopSearchBar.js
+++ src/components/shop/shopSearchBar.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 function FormSearchBar(props) {
   return (
-    <input
-      {...props.input}
-      type="text"
-      placeholder={props.placeholder}
-      className={`${props.className} form-search-bar`}
-    />
+    <div className='search-bar-grid'>
+      <FontAwesomeIcon className='search-bar-grid__icon' icon={faSearch} />
+      <input
+        {...props.input}
+        type="text"
+        placeholder={props.placeholder}
+        className={`${props.className} form-search-bar search-bar-grid__input`}
+      />
+    </div>
   )
 }
 
